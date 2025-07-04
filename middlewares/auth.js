26579427cdc7891692d633f8055259f1f66a8cb4
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-04 19:55:17 +0330

    verifying access tokens from Authorization header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-function auth(req, res, next) {
-  const token = req.header("authorization");
+function auth (req,res,next) {
+    const authHeader  = req.headers["authorization"];
+    if(!authHeader) return res.status(401).send("access token not provided");
 
-  if (!token) return res.status(401).send("access denied");
-  try {
-    const decode = jwt.verify(token, process.env.SECRET_KEY);
-    req.userData = decode;
-    next();
-  } catch (error) {
-    return res.status(400).send("token is invalid");
-  }
+  const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(403).send("Invalid or expired access token")
+    }
 }
 
-module.exports = auth;
+module.exports = {auth};
