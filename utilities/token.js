9e1727845573d33eff9fd6dd7f6656a325b66de7
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-04 20:04:19 +0330

    implement access and refresh token generation with separate secrets

---
+++ utilities/token.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign(
+    { id :userId },
+    process.env.ACCESS_TOKEN_SECRET, // کلید جداگانه برای access token
+    { expiresIn: "15m" } // 15 دقیقه اعتبار
+  );
+
+  const refreshToken = jwt.sign(
+    { id :userId },
+    process.env.REFRESH_TOKEN_SECRET, // کلید جدا برای refresh token
+    { expiresIn: "7d" } // 7 روز اعتبار
+  );
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 روز بعد
+
+  return { accessToken, refreshToken , expiresAt };
+};
+
+module.exports = { generateTokens };
