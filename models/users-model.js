680764d3bcf43f5dfcd36ed0e157e1ae04bfed64
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-04 19:51:01 +0330

    add getUserById function to retrieve user ID for token storage

--- models/users-model.js
+++ models/users-model.js
@@ -1,9 +1,9 @@
-const pool = require("../config/db_mysql");
+const {pool} = require("../config/db_mysql");
 
 class userModel {
   static insertUser = async (name, email, password) => {
-    const result = pool.query(
-      "insert into test.test_tb(name ,email ,password) values(?,?,?) ",
+    const result = await pool.query(
+      "insert into demo.users(name ,email ,password) values(?,?,?) ",
       [name, email, password]
     );
     return result;
@@ -11,8 +11,16 @@ class userModel {
 
   static getEmail = async (email) => {
     const [result] = await pool.query(
-      "SELECT * FROM test.test_tb WHERE email = ?",
+      "SELECT * FROM demo.users WHERE email = ?",
       [email]
+    );
+    return result[0];
+  };
+
+  static getUserById = async (id) => {
+    const [result] = await pool.query(
+      "SELECT * FROM demo.users WHERE id = ?",
+      [id]
     );
     return result[0];
   };
