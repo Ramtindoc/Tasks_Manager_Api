4c9e7a0cd3812852329bde92faa7d449311b9730
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-06 20:04:31 +0330

    add a few comments

--- router/auth-router.js
+++ router/auth-router.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { limiter, register, login} = require('../controller/user-controller');
+const { limiter, register, login } = require("../controller/user-controller");
 const { refreshToken } = require("../controller/auth-controller");
 
-router.post('/register',register);
-router.post('/login', login ,limiter);
-router.post('/refresh-token',refreshToken)
+// routes
+router.post("/register", register);
+router.post("/login", login, limiter);
+router.post("/refresh-token", refreshToken);
 
 module.exports = router;
--- router/users_router.js
+++ router/users_router.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require("../models/users-model")
-const {auth} = require("../middlewares/auth");
+const userModel = require("../models/users-model");
+const { auth } = require("../middlewares/auth");
 
+// find user by ID
+router.get("/profile", auth, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await userModel.getUserById(userId);
 
-router.get('/profile', auth , async (req,res)=> {
-    try {
-        const userId = req.user.id;
-        const user = await userModel.getUserById(userId);
-
-        if(!user) return res.status(404).send("user not found");
-        
-        else 
-        return res.status(200).send(
-            {
-                email:user.email,
-                name:user.name,
-            }
-        );
-    } catch (err) {
-        res.status(500).send('server error')
-    }
-})
+    if (!user) return res.status(404).send("user not found");
+    else
+      return res.status(200).send({
+        email: user.email,
+        name: user.name,
+      });
+  } catch (err) {
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
