e3be288b972757bc3f4a3e94c891d9742de20cb4
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-06 20:00:48 +0330

    delete some self notes in source

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -6,67 +6,67 @@ const jwt = require("jsonwebtoken");
 const { tryCatchHandller } = require("../utilities/trycatch_handller");
 const appError = require("../utilities/app_error");
 const logger = require("../logger/logger");
-const rateLimit = require('express-rate-limit');
-const TokenModel = require("../models/token-model")
-const {generateTokens} = require("../utilities/token")
+const rateLimit = require("express-rate-limit");
+const TokenModel = require("../models/token-model");
+const { generateTokens } = require("../utilities/token");
 require("dotenv").config();
 
 // rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 10,
-  message: 'تعداد درخواست‌های شما بیش از حد مجاز است. لطفاً بعداً دوباره تلاش کنید.',
-  standardHeaders: true, 
-  legacyHeaders: false, 
+  message:
+    "تعداد درخواست‌های شما بیش از حد مجاز است. لطفاً بعداً دوباره تلاش کنید.",
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
 //  rergister
 const register = tryCatchHandller(async (req, res) => {
   const schema = {
-    
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().min(5).max(100).required(),
     password: Joi.string().min(5).max(100).required(),
-    
   };
   // validate
   const validateResult = Joi.object(schema).validate(req.body);
 
   if (validateResult.error) throw validateResult.error;
 
-  // controll repeat to send data by email
+  // get email from user model
   const user = await userModel.getEmail(req.body.email);
   if (user) {
-    // return res.status(400).send("user is already register!");
     throw new appError(100, "user is already register", 400);
   }
+
   const hashPass = await bcrypt.hash(req.body.password, 10);
-  // insert at body requests and writen in model module
+
+  // insert into dbms
   const result = await userModel.insertUser(
     req.body.name,
     req.body.email,
     hashPass
-    // req.body.password    //test
   );
   logger.register(result);
 
   // new user
   const newUser = await userModel.getEmail(req.body.email);
 
-  // return create a new user and is show just name and email and send in frontend just this
-  // user 'lodash' to get details like name and email
-  const token = jwt.sign({ id: newUser.id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+  const token = jwt.sign({ id: newUser.id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
   return res
     .header("authorization", token)
-    .send(_.pick(newUser, "name", "email"))});
+    .send(_.pick(newUser, "name", "email"));
+});
 
-// ----- login
-
+//login
 const login = tryCatchHandller(async (req, res) => {
   const schema = {
     email: Joi.string().email().min(5).max(100).required(),
     password: Joi.string().min(5).max(100).required(),
   };
+
   // validate
   const validateResult = Joi.object(schema).validate(req.body);
 
