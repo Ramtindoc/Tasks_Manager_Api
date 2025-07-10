228890e8d047d8331353a1700e02332b2efd5a1c
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-10 21:05:52 +0330

    some is changed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 
-function auth (req,res,next) {
-    const authHeader  = req.headers["authorization"];
-    if(!authHeader) return res.status(401).send("access token not provided");
+function auth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).send("access token not provided");
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
-    try {
-        const decoded = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.status(403).send("Invalid or expired access token")
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).send("Invalid or expired access token");
+  }
 }
 
-module.exports = {auth};
+module.exports = { auth };
