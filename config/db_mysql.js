5c9c136c2789b692c5f13f09f2370f44ac0f1319
Ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-07-26 15:40:50 +0330

    used sequelize added

--- config/db_mysql.js
+++ config/db_mysql.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const pool =  mysql
+const pool = mysql
   .createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
@@ -10,37 +10,33 @@ const pool =  mysql
   })
   .promise();
 
-  // Store token
-async function storeToken(userId,accessToken,refreshToken,expiresAt) {
- 
+// Store token
+async function storeToken(accessToken, refreshToken, expiresAt) {
   try {
-
     await pool.execute(
-  'INSERT INTO demo.user_tokens (user_id, access_token, refresh_token, created_at, expires_at) VALUES (?, ?, ?, ? ,?)',
-  [userId, accessToken, refreshToken, new Date(),expiresAt]
-);
- } catch (err) {
-    console.error('error storing tokens',err);
-    throw err
- }
- }
+      "INSERT INTO demo.tokens (access_token, refresh_token, expires_at) VALUES (?, ?, ?)",
+      [accessToken, refreshToken, expiresAt]
+    );
+  } catch (err) {
+    console.error("error storing tokens", err);
+    throw err;
+  }
+}
 
 // Read token
 async function getTokenByRefreshToken(refreshToken) {
   // const [rows,fields]
   try {
-    const[rows] = await pool.execute (
-  'SELECT * FROM demo.user_tokens WHERE refresh_token = ?',
-  [refreshToken]
-);
+    const [rows] = await pool.execute(
+      "SELECT * FROM demo.tokens WHERE refresh_token = ?",
+      [refreshToken]
+    );
 
-return rows[0];
-
+    return rows[0];
   } catch (err) {
-    console.error('Error fetching token',err);
+    console.error("Error fetching token", err);
     throw err;
   }
-} 
+}
 
-
-module.exports ={pool,storeToken,getTokenByRefreshToken};
+module.exports = { pool, storeToken, getTokenByRefreshToken };
